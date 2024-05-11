0ae8a8310cede6d79cb9517f8351a66191cc96dd
Juan M C <juanmcutrera@unbambu.com>
2024-05-11 01:03:23 -0300

    contact fields colors

--- src/app/shared/components/contact/contact.component.ts
+++ src/app/shared/components/contact/contact.component.ts
@@ -16,12 +16,12 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import { NgClass } from '@angular/common';
 
 /** Error when invalid control is dirty, touched, or submitted. */
-/*export class MyErrorStateMatcher implements ErrorStateMatcher {
+export class MyErrorStateMatcher implements ErrorStateMatcher {
   isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
     const isSubmitted = form && form.submitted;
     return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
   }
-}*/
+}
 
 @Component({
   selector: 'app-contact',
@@ -78,7 +78,7 @@ export class ContactComponent implements OnInit {
     // continue work here
   }
  
-  //matcher = new MyErrorStateMatcher();
+  matcher = new MyErrorStateMatcher();
 
 
 }
